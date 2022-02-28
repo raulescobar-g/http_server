9ed356f2d8c2a928ae7df60f270006e86812e507
raulescobar-g <raulescobar_g@tamu.edu>
2022-02-28 11:22:36 -0600

    comments

--- server/src/main.go
+++ server/src/main.go
@@ -13,20 +13,25 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
+// body content
 type userResponse struct {
 	Name   string   `json:"name"`
 	Colors []string `json:"colors"`
 }
+
+//body content on errors
 type errorResponse struct {
 	Error string `json:"error"`
 }
 
+// ping handler
 func handlePing(w http.ResponseWriter, r *http.Request) {
-	log.Println(fmt.Sprintf("%s %s", r.Method, r.URL.Path)) //log path and method
+	log.Println(fmt.Sprintf("%s %s", r.Method, r.URL.Path))
 }
 
+// handler for /user path
 func handleUser(w http.ResponseWriter, r *http.Request) {
-	log.Println(fmt.Sprintf("%s %s", r.Method, r.URL.Path)) //log path and method
+	log.Println(fmt.Sprintf("%s %s", r.Method, r.URL.Path))
 	conn, err := getConn()
 
 	if errorFound(err, "connecting to database", http.StatusInternalServerError, w) {
@@ -34,17 +39,25 @@ func handleUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	switch r.Method {
+	//  path: /user?user_id=<int>  ==> body on response {name:<string>, colors:[<string>,...]}
 	case http.MethodGet:
 		w.Header().Set("Content-Type", "application/json")
 
-		personId := r.URL.Query().Get("id")
+		userId := r.URL.Query().Get("user_id")
 
-		rows, err := conn.Query("SELECT name,color FROM users JOIN users_colors ON users.id = users_colors.user_id AND id=$1;", personId)
-		defer rows.Close()
-
-		if errorFound(err, fmt.Sprintf("querying name and color with id=%s", personId), http.StatusBadRequest, w) {
+		if userId == "" {
+			errorFound(sql.ErrNoRows, "getting input parameter", http.StatusBadRequest, w)
 			return
 		}
+
+		rows, err := conn.Query("SELECT name,color FROM users JOIN users_colors ON users.id = users_colors.user_id AND id=$1;", userId)
+
+		if errorFound(err, fmt.Sprintf("querying name and color with id=%s", userId), http.StatusBadRequest, w) {
+			return
+		}
+
+		// this must go after error checking the rows, or else will close a nil pointer
+		defer rows.Close()
 
 		responseStruct := userResponse{}
 		var name, color string
@@ -60,27 +73,31 @@ func handleUser(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		// on nothing found return no rows sql error
 		if responseStruct.Name == "" {
 			errorFound(sql.ErrNoRows, "returning result", http.StatusNotFound, w)
 			return
 		}
-
+		// write to response json
 		json.NewEncoder(w).Encode(responseStruct)
 
 		return
 
 	case http.MethodPost:
-		// Create a new record.
+		// Create a new record. not implemented
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	case http.MethodPut:
-		// Update an existing record.
+		// Update an existing record. not implemented
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	case http.MethodDelete:
-		// Remove the record.
+		// Remove the record. not implemented
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
-
 }
 
+// gets db connection, tests the connection and returns Result pair
 func getConn() (*sql.DB, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("HOST"), os.Getenv("PORT_DB"), os.Getenv("USER_DB"), os.Getenv("PASSWORD"), os.Getenv("DB_NAME"))
 	db, err := sql.Open("pgx", psqlInfo)
@@ -96,6 +113,8 @@ func getConn() (*sql.DB, error) {
 	return db, nil
 }
 
+// on err, will write status code into response, log the expected behavior with error, and write the error to the body
+// returns true if err not nill
 func errorFound(err error, expect string, statusCode int, w http.ResponseWriter) bool {
 	if err != nil {
 		w.WriteHeader(statusCode)
@@ -109,17 +128,19 @@ func errorFound(err error, expect string, statusCode int, w http.ResponseWriter)
 
 func main() {
 	err := godotenv.Load()
+
+	// if cant load db credentials, log it, and exit
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	port := os.Getenv("PORT")
 
-	//using default mux
+	//using default mux for paths
 	http.HandleFunc("/", handlePing)
 	http.HandleFunc("/user", handleUser)
 
+	// log on server listening and server exit
 	log.Println("Running on port", port)
 	log.Fatal(http.ListenAndServe(port, nil))
-
 }

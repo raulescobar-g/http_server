ff2437dd64c09632c5af2f58e5736aca06c1fa76
raulescobar-g <raulescobar_g@tamu.edu>
2022-02-27 19:43:48 -0600

    implemented get request

--- server/src/main.go
+++ server/src/main.go
@@ -4,55 +4,70 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
-)
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "Chispis1#"
-	dbname   = "users"
 )
 
 type userResponse struct {
 	Name   string   `json:"name"`
 	Colors []string `json:"colors"`
 }
+type errorResponse struct {
+	Error string `json:"error"`
+}
 
-func handlePing(w http.ResponseWriter, r *http.Request) {}
+func handlePing(w http.ResponseWriter, r *http.Request) {
+	log.Println(fmt.Sprintf("%s %s", r.Method, r.URL.Path)) //log path and method
+}
 
 func handleUser(w http.ResponseWriter, r *http.Request) {
-	log.Println(fmt.Sprintf("%s %q", r.Method, html.EscapeString(r.URL.Path))) //basic logging
-	conn := getConn()
+	log.Println(fmt.Sprintf("%s %s", r.Method, r.URL.Path)) //log path and method
+	conn, err := getConn()
+
+	if errorFound(err, "connecting to database", http.StatusInternalServerError, w) {
+		return
+	}
 
 	switch r.Method {
 	case http.MethodGet:
 		w.Header().Set("Content-Type", "application/json")
 
 		personId := r.URL.Query().Get("id")
+
 		rows, err := conn.Query("SELECT name,color FROM users JOIN users_colors ON users.id = users_colors.user_id AND id=$1;", personId)
-		if err != nil {
-			log.Fatalf(fmt.Sprintf("Error querying user colors with error: %s", err)) // here return error response
+		defer rows.Close()
+
+		if errorFound(err, fmt.Sprintf("querying name and color with id=%s", personId), http.StatusBadRequest, w) {
+			return
 		}
 
 		responseStruct := userResponse{}
 		var name, color string
-		for rows.Next() {
-			err = rows.Scan(&name, &color)
-			if err != nil {
-				log.Fatalf(fmt.Sprintf("Could not read user with error: %s", err))
-			}
 
+		for rows.Next() {
+			rows.Scan(&name, &color)
 			responseStruct.Name = name
 			responseStruct.Colors = append(responseStruct.Colors, color)
-
 		}
+
+		if rows.Err() != nil {
+			errorFound(rows.Err(), "reading rows", http.StatusInternalServerError, w)
+			return
+		}
+
+		if responseStruct.Name == "" {
+			errorFound(sql.ErrNoRows, "returning result", http.StatusNotFound, w)
+			return
+		}
+
 		json.NewEncoder(w).Encode(responseStruct)
+
+		return
 
 	case http.MethodPost:
 		// Create a new record.
@@ -66,23 +81,39 @@ func handleUser(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func getConn() *sql.DB {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+func getConn() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("HOST"), os.Getenv("PORT_DB"), os.Getenv("USER_DB"), os.Getenv("PASSWORD"), os.Getenv("DB_NAME"))
 	db, err := sql.Open("pgx", psqlInfo)
+
 	if err != nil {
-		log.Fatalf("could not connect to database: %v", err)
+		return nil, err
 	}
 
 	if err := db.Ping(); err != nil {
-		log.Fatalf("unable to reach database: %v", err)
+		return nil, err
 	}
-	//log.Println("Successfully connected to database") debug statement
 
-	return db
+	return db, nil
+}
+
+func errorFound(err error, expect string, statusCode int, w http.ResponseWriter) bool {
+	if err != nil {
+		w.WriteHeader(statusCode)
+		log.Println(fmt.Sprintf("%s: %s", expect, err.Error()))
+		responseStruct := errorResponse{Error: err.Error()}
+		json.NewEncoder(w).Encode(responseStruct)
+		return true
+	}
+	return false
 }
 
 func main() {
-	port := ":8080"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
 
 	//using default mux
 	http.HandleFunc("/", handlePing)
